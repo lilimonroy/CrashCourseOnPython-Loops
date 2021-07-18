cbed2c1a7642c610679bac2ed9fbd1ee3c91a196
Lili Monroy <galadriel@comunidad.unam.mx>
2021-07-18 17:19:00 -0500

    fetch: Update file

--- q2LoopFinal.py
+++ q2LoopFinal.py
@@ -1,4 +1,4 @@
-#4.Question 4
+#Question 4
 
 #This function prints out a multiplication table (where each number is the result of multiplying the first number of its row by the number at the top of its column). 
 #Fill in the blanks so that calling multiplication_table(1, 3) will print out:
